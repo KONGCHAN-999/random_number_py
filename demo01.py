7fdefafc6f3ebecc77220c6eee414fab42c03dde
seonekr <93129992+KONGCHAN-999@users.noreply.github.com>
2024-07-08 15:27:03 +0700

    Update

---
+++ demo01.py
@@ -0,0 +1,6 @@
+import random
+
+# Generate a list of 10 random integers between 1 and 100
+random_numbers = [random.randint(1, 1000000) for _ in range(1000)]
+
+print("List of 10 random numbers:", random_numbers)
